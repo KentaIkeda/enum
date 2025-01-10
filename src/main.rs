1006e16a9a57f5418bca0541188fbd13bd6509cd
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2025-01-10 22:37:54 +0900

    enumについて学習

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// enumキーワードを使用して、列挙型を作成
+enum IpAddressKind {
+    V4,
+    V6
+}
+
 fn main() {
-    println!("Hello, world!");
+    // enumを使用する際には::演算子を使用して、アクセスする
+    // IpAddressKind型を受け取る関数にパラメータとして渡せる
+    let four = IpAddressKind::V4;
+    let six = IpAddressKind::V6;
+    // こんな感じで
+    route(four);
+    route(six);
+}
+
+fn route(ip: IpAddressKind) -> IpAddressKind {
+    ip
 }
