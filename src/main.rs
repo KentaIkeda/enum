94d2eaad1adcbcc86e141136cd6d66e70a208dc1
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2025-01-12 23:18:45 +0900

    daily

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ enum IpAddressKind {
     V6
 }
 
+struct IpAddressStructure {
+    kind: IpAddressKind,
+    address: String
+}
+
 fn main() {
     // enumを使用する際には::演算子を使用して、アクセスする
     // IpAddressKind型を受け取る関数にパラメータとして渡せる
@@ -12,6 +17,15 @@ fn main() {
     // こんな感じで
     route(four);
     route(six);
+
+    let home = IpAddressStructure {
+        kind: IpAddressKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    let loopback = IpAddressStructure {
+        kind: IpAddressKind::V6,
+        address: String::from("::1"),
+    };
 }
 
 fn route(ip: IpAddressKind) -> IpAddressKind {
