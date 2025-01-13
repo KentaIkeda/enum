c9c3ca52efb9e2f35a5497b761f9b3e81b96bffb
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2025-01-13 22:42:34 +0900

    daily

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,30 @@
 // enumキーワードを使用して、列挙型を作成
-enum IpAddressKind {
-    V4,
-    V6
+// 型が違くてもOK!
+// enum IpAddressKind {
+//     V4(u8, u8, u8, u8),
+//     V6(String)
+// }
+
+// 奇麗なバージョン
+struct IpV4Address {
+    
+}
+struct IpV6Address {
+
 }
 
-struct IpAddressStructure {
-    kind: IpAddressKind,
-    address: String
+enum IpAddress {
+    V4(IpV4Address),
+    V6(IpV6Address)
 }
 
 fn main() {
     // enumを使用する際には::演算子を使用して、アクセスする
     // IpAddressKind型を受け取る関数にパラメータとして渡せる
-    let four = IpAddressKind::V4;
-    let six = IpAddressKind::V6;
-    // こんな感じで
-    route(four);
-    route(six);
-
-    let home = IpAddressStructure {
-        kind: IpAddressKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-    let loopback = IpAddressStructure {
-        kind: IpAddressKind::V6,
-        address: String::from("::1"),
-    };
+    let home = IpAddressKind::V4(127, 0, 0, 1);
+    let loopback = IpAddressKind::V6(String::from("::1"));
+    route(home);
+    route(loopback);
 }
 
 fn route(ip: IpAddressKind) -> IpAddressKind {
